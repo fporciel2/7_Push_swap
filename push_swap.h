4c58d06b02c681d7f526016fc780dc55ae040e9a
fporciel <fporciel@student.42roma.it>
2023-11-08 12:01:42 +0100

    Is reality a dream?

---
+++ push_swap.h
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fporciel <fporciel@student.42roma.it>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/08 11:41:31 by fporciel          #+#    #+#             */
+/*   Updated: 2023/11/08 12:00:26 by fporciel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+/*   
+* Push_swap is a versatile sorting algorithm visualization tool. Designed as
+*  a part of a programming challenge, they demonstrate the efficiency of
+*  different sorting algorithms by visually displaying their operations.
+*  The Push_swap and Checker programs are designed to work together for
+*  sorting integers using a set of specific instructions while visualizing the
+*  sorting process. Push_swap generates a sequence of instructions to sort a
+*  list of numbers, and Checker verifies whether these instructions are valid
+*  and successfully sort the list.
+*  Copyright (C) 2023  Federico Porciello
+*
+*  This program is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+# include "./1_libft/libft.h"
+# include "./2_ft_printf/ft_printf.h"
+
+typedef struct s_stack
+{
+	int				value;
+	size_t			position;
+	size_t			correct_position;
+	struct s_stack	*next;
+}					t_stack;
+
+struct
+{
+	size_t	pb;
+	size_t	pa;
+	size_t	sa;
+	size_t	sb;
+	size_t	ss;
+	size_t	ra;
+	size_t	rb;
+	size_t	rr;
+	size_t	rra;
+	size_t	rrb;
+	size_t	rrr;
+}			s_op;
+
+typedef struct s_ps
+{
+	t_stack		*a;
+	t_stack		*b;
+	struct s_op	*ops;
+}				t_ps;
+
+#endif
